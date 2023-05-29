0d78820a80b0800161dfca9af7ca155e10abc074
Dreamer <117353711+TonyDo99@users.noreply.github.com>
2023-05-29 15:57:56 +0700

    refure cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000, () =>
     console.log('Server now listening on port 3000 !'),
